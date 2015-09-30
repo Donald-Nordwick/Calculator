6e9c14a154b98751ebd2ab1a68a735e8cdeae54f
SAC-CS112-Nordwick-Donald <donnordwick@gmail.com>
2015-09-29 23:37:50 -0700

    Update Calculator.java

--- Calculator/src/Calculator.java
+++ Calculator/src/Calculator.java
@@ -8,11 +8,13 @@ public class Calculator {
 	{
 		Scanner input = new Scanner( System.in);
 		
+	
 		double num3 = 0;
+		double num5=0;
 		
 		System.out.println("Enter your first number:");
 		double num1= input.nextDouble();
-		
+	
 		System.out.println("Enter your Second number number:");
 		double num2 = input.nextDouble();
 		
@@ -22,22 +24,59 @@ public class Calculator {
 		
 		if (op == 1){
 			num3 = num1 * num2;
-			System.out.printf( "%.4f * %.4f = %.4f", num1 , num2, num3 );
+			System.out.printf( "%.4f * %.4f = %.4f\n", num1 , num2, num3 );
 		}
 		else if  (op==2){
 			num3 = num1 / num2;
-			System.out.printf( "%.4f / %.4f = %.4f", num1 , num2, num3 );
+			System.out.printf( "%.4f / %.4f = %.4f\n", num1 , num2, num3 );
 		}
 		else if  (op==3){
 			num3 = num1 + num2;
-			System.out.printf( "%.4f / %.4f = %.4d", num1 , num2, num3 );
+			System.out.printf( "%.4f + %.4f = %.4f\n", num1 , num2, num3 );
 		}
 		else if  (op==4){
 			num3 = num1 - num2;
-			System.out.printf( "%.4d / %.4d = %.4d", num1 , num2, num3 );
+			System.out.printf( "%.4f - %.4f = %.4f\n", num1 , num2, num3 );
 		}
 		else{
 			System.out.println(" Operation not recognized.");
 		}
+		System.out.println("Would you like to keep calculating?\nEnter 1 for yes");
+		int ans = input.nextInt();
+		while(ans==1)
+		{
+			System.out.println("Please enter your next number");
+			double num4 = input.nextDouble();
+			System.out.println("Enter the operation you would like to do:\nFor multiplication use 1");
+			System.out.println("For division use 2\nFor addition use 3\nFor subtration use 4");
+			op = input.nextInt();
+			
+			if (op == 1){
+				num5=num3;
+				num3 = num3 * num4;
+				System.out.printf( "%.4f * %.4f = %.4f\n", num5 , num4, num3 );
+			}
+			else if  (op==2){
+				num5=num3;
+				num3 = num3 / num4;
+				System.out.printf( "%.4f / %.4f = %.4f\n", num5 , num4, num3 );
+			}
+			else if  (op==3){
+				num5=num3;
+				num3 = num3 + num4;
+				System.out.printf( "%.4f + %.4f = %.4f\n", num5 , num4, num3 );
+			}
+			else if  (op==4){
+				num5=num3;
+				num3 = num3 - num4;
+				System.out.printf( "%.4f - %.4f = %.4f\n", num5 , num4, num3 );
+			}
+			else{
+				System.out.println(" Operation not recognized.");
+			}
+			System.out.println("Would you like to keep calculating?\nEnter 1 for yes");
+			ans = input.nextInt();
+		}
+			
 	}
 }
